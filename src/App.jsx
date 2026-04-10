c73b24fda1bc21ebd5041a4391e8f9072369d130
Luiz Fellipy G. Roza <luizfellipy235@gmail.com>
2026-04-10 00:23:23 -0300

    add lógica filtros

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
+import { useState } from "react"
 import { Carditem } from "./components/CardItem.jsx"
 import data from "./data.js"
 
 function App() {
+  const [filtroConfederacao, setFiltroConfederacao] = useState(null)
+  const [filtroTitulo, setFiltroTitulo] = useState(null)
+
   return (
-    <div className="flex flex-col items-center">
-      <h1>CATÁLOGO COPA DO MUNDO</h1>
+    <div className="flex flex-col items-center gap-5 mt-4">
+      <h1 className="text-xl font-semibold font">CATÁLOGO COPA DO MUNDO</h1>
 
 
 
       <div className="flex gap-4 flex-wrap">
-        {data.map((time) => {
+        {data.filter((time) => {
+          return !filtroConfederacao || time.confederacao === filtroConfederacao
+        }).filter((time) => {
+          return filtroTitulo === null || time.titulosCopa === filtroTitulo
+        }).map((time) => {
         return (<Carditem
         id={time.id}
         nome={time.nome}
