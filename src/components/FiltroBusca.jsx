c29dd36648fb639b55fffcd856c6727ac6e0776a
BrenoMoreiraAds <bnmoreirasoares@gmail.com>
2026-04-17 19:57:21 -0300

    estilizando filtro de busca

--- src/components/FiltroBusca.jsx
+++ src/components/FiltroBusca.jsx
@@ -1,10 +1,14 @@
 export function FiltroBusca(props){
     return (
         <div className="flex flex-col items-center gap-2">
-            <h1>NOME</h1>
+            <h1 className="text-ouro-medio font-medium">NOME</h1>
             <input value={props.filtro} placeholder="Ex: Brasil" type="text"
             onChange={(e) => props.setFiltro(e.target.value)}
-            className={`border border-black rounded-full p-2 ${props.filtro !== "" ? "bg-yellow-500" : ""}`}></input>
+            className={`border rounded-full px-4 py-2 font-medium outline-none bg-trofeu-bg-escuro text-ouro-brilhante placeholder:text-trofeu-text-outro
+          ${props.filtro !== ""
+            ? "border-ouro-medio"
+            : "border-ouro-escuro/40 hover:border-ouro-brilhante focus:border-ouro-medio"
+          }`}></input>
         </div>
     )
 }
