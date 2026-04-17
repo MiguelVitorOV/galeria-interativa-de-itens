84afdd5d9d28dd5414c7efa683b7b3762de44d94
BrenoMoreiraAds <bnmoreirasoares@gmail.com>
2026-04-17 19:11:55 -0300

    add paleta de cores no tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ouro: {
+          escuro:   '#8B6914',
+          medio:    '#C9950A',  
+          brilhante: '#E8B923',  
+        },
+        trofeu: {
+          'bg-preto':    '#0D0B08',  
+          'bg-escuro': '#1C1812', 
+          'text-claro': '#F5E9C8',  
+          'text-escuro':  '#2A1F08', 
+          'text-outro': '#8A7650',  
+        },
+      },
+    },
   },
   plugins: [],
 }
-
